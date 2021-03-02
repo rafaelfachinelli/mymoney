afd6a3e50cd08c9e1fbde55a3aeecdec178f3782
rafaelfachinelli <rafael.fachinelli@fatec.sp.gov.br>
2021-03-02 09:45:54 -0300

    ci: api finished

--- backend/src/api/billingCycle/billingCycleService.js
+++ backend/src/api/billingCycle/billingCycleService.js
@@ -1,7 +1,10 @@
 const BillingCycle = require('./billingCycle');
+const errorHandler = require('../common/errorHandler');
 
 BillingCycle.methods(['get', 'post', 'put', 'delete']);
 BillingCycle.updateOptions({ new: true, runValidators: true });
+BillingCycle.after('post', errorHandler);
+BillingCycle.after('put', errorHandler);
 
 BillingCycle.route('get', (req, res, next) => {
   BillingCycle.find({}, (err, docs) => {
@@ -9,6 +12,32 @@ BillingCycle.route('get', (req, res, next) => {
       res.json(docs);
     } else {
       res.status(500).json({errors: [error]});
+    }
+  });
+});
+
+BillingCycle.route('count', (req, res, next) => {
+  BillingCycle.count((error, value) => {
+    if (error) {
+      res.status(500).json({ errors: [error] });
+    } else {
+      res.json({ value });
+    }
+  })
+});
+
+BillingCycle.route('summary', (req, res, next) => {
+  BillingCycle.aggregate([{ 
+    $project: {credit: {$sum: "$credits.value"}, debt: {$sum: "$debts.value"}} 
+  }, { 
+    $group: {_id: null, credit: {$sum: "$credit"}, debt: {$sum: "$debt"}}
+  }, { 
+    $project: {_id: 0, credit: 1, debt: 1}
+  }], (error, result) => {
+    if(error) {
+      res.status(500).json({errors: [error]})
+    } else {
+      res.json(result[0] || {credit: 0, debt: 0})
     }
   });
 });
