8015f32849741975e0d5c2a15ffa416088ae0b98
rafaelfachinelli <rafael.fachinelli@fatec.sp.gov.br>
2021-03-02 10:01:15 -0300

    ci: cors added

--- backend/src/config/server.js
+++ backend/src/config/server.js
@@ -1,11 +1,13 @@
 const bodyParser = require('body-parser');
 const express = require('express');
 const server = express();
+const cors = require('./cors');
 
 const port = 3003;
 
 server.use(bodyParser.urlencoded({extended: true}));
 server.use(bodyParser.json());
+server.use(cors);
 
 server.listen(port, function() {
   console.log(`BACKEND is running on port ${port}`);
