75073690b684e1bb44fdfd61dd559f18830a2c8c
rafaelfachinelli <rafael.fachinelli@fatec.sp.gov.br>
2021-03-03 08:18:19 -0300

    feat(footer): component finished and added in app

---
+++ frontend/src/common/template/footer.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default (props) => (
+  <footer className="main-footer">
+    <strong>
+      Copyright © 2021
+      <a href="https://www.github.com/rafaelfachinelli/mymoney" target="_blank">
+        {" "}
+        Rafael Fachinelli
+      </a>
+      .
+    </strong>
+  </footer>
+);
